48d05732b685c54984189e5578483980c08fb893
Bilal <solidhydrogenblock@gmail.com>
2021-04-01 07:14:34 -0700

    different location for config

--- audit_log/app.py
+++ audit_log/app.py
@@ -7,7 +7,7 @@ import json
 from pykafka import KafkaClient
 from pykafka.common import OffsetType
 from flask_cors import CORS, cross_origin
-# import os
+import os
 # import pymysql
 # import datetime
 
@@ -19,10 +19,19 @@ from flask_cors import CORS, cross_origin
 # from threading import Thread
 
 
-with open('app_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
